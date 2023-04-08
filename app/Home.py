2550c236f02f5ac2a4ac56d264dfab18a03af7d1
DanielBessadev <danielbessadev@gmail.com>
2023-04-07 23:28:13 -0300

    home

---
+++ app/Home.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+# Streamlit
+st.set_page_config(page_title='Meu Setup', layout='wide')
+
+st.title("Meu Setup")
+
+st.write("""
+    O acesso de pessoas físicas no mercado financeiro cresce continuamente. A abordagem Quant direciona e auxilia o trader a minimizar o risco nas operações no mercado. O backtest de setups nos diversos ativos é uma ferramenta importante para a tomada de decisões.")
+    
+    Essa ferramenta foi projetada e estruturada em várias páginas que podem ser acessadas usando a barra lateral. Cada uma das páginas aborda uma maneira de analisar o backtest de setups.
+        Filtrar o ativo e comparar os diversos setups;
+	    Filtrar o setup e comparar os diversos ativos;
+	    Analisar a performance do setup e do ativo em maior detalhe.
+
+    Os trades são realizados utilizando todos os dados do ativo e posteriormente filtrados para o período a ser analisado.
+
+    Setups tem entrada única (não é feito multitrading).
+
+    Os trades são feitos somente na ponta da compra.
+
+    Todos os valores dos ativos e estatísticas estão cotadas em Real Brasileiro (BRL).""")
+
+st.subheader("Mercado Financeiro")
+st.write("""
+    Ativos listados na [**B3**](https://www.b3.com.br/pt_br/) e baixados através da biblioteca de automação de navegadores [**Selenium**](https://www.selenium.dev/pt-br/documentation/).
+    
+    Dados de mercado baixados da [**API Yahoo! de Finanças**](https://pypi.org/project/yfinance/).
+
+    Maioria dos Setups divulgados pelo trader **Stormer** da [**L&S**](https://ls.com.vc).""")
+
+st.subheader("Ciência de Dados e Engenharia de Dados")
+st.write("""
+    Transformações e cálculos implementados usando as bibliotecas [**Pandas**](https://pandas.pydata.org/docs/reference/index.html) e [**Numpy**](https://numpy.org/doc/stable/).
+
+    Visualização gráfica realizada com a biblioteca [**Graph Objects (go)**](https://plotly.com/python/graph-objects/) em plotly Python.
+
+    Dashboard escrito em Python usando o framework web Streamlit. (https://docs.streamlit.io/library/api-reference)""")
+
+st.write("Isenção de responsabilidade: Não é uma recomendação de compra ou venda de ativos no mercado financeiro.")
---
+++ app/pages/Já_sei_meu_Setup.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+
+from functions.functions import all_stocks_setup_report
+from functions.graphs import graph_compare_report
+from functions.report import report_columns
+from functions.setups import frequency, setups_info
+from functions.tickers import tickers
+
+import streamlit as st
+
+# Streamlit
+st.set_page_config(page_title='Meu Setup', layout='wide')
+
+st.subheader("Performance do meu setup em todos os ativos")
+
+c1, c2, c3, c4, c5, c6, c7 = st.columns(7)
+with c1:
+    start_date = st.date_input('Data inicial', value=datetime.strptime('2016-01-01', '%Y-%m-%d'))
+with c2:
+    end_date = st.date_input('Data Final')
+with c3:
+    inp_frequency = st.selectbox('Frequência', frequency.keys())
+    folder = frequency[inp_frequency]
+with c4:
+    if (folder == 'wk'):
+        inp_setup = st.selectbox('Setup', setups_info.keys())
+        setup = str(setups_info[inp_setup] + '_wk')
+    elif (folder == 'd'):
+        inp_setup = st.selectbox('Setup', setups_info.keys())
+        setup = str(setups_info[inp_setup] + '_d')
+with c5:
+    start_capital = st.number_input(label='Capital Inicial', value=10000, step=1000, format='%d')
+with c6:
+    trade_cost = st.number_input(label='Custo Operacional', value=6, step=1, format='%d')
+with c7:
+    risk = st.checkbox(label='Patrimônio Reinvestido', value=False)
+
+all_stocks_setup_report = all_stocks_setup_report(tickers, start_date=start_date, end_date=end_date, folder=folder, setup=setup, risk=risk, start_capital=start_capital, trade_cost=trade_cost)
+
+see_data = st.expander('Clique aqui para ver todos os dados 👉')
+with see_data:
+    st.dataframe(all_stocks_setup_report.style.format({
+        # Basic Report
+        'Stock': "{:}",
+        'Starting Capital': "{:.2f}",
+        'Trade Cost': "{:.2f}",
+        'Total Trade Cost': "{:.2f}",
+        'Ending Capital': "{:.2f}",
+        'Net Profit Value': "{:.2f}",
+        'Net Profit %': "{:.2f}",
+        'Start Date': "{:}",
+        'End Date': "{:}",
+        'Annualized Profit %': "{:.2f}",
+        'Annual Volatility %': "{:.2f}",
+        # General Trades
+        'Number of Trades': "{:.0f}",
+        'Active Trade': "{:}",
+        'Average Return Value': "{:.2f}",
+        'Average Return %': "{:.2f}",
+        'Gain/Loss Ratio': "{:.2f}",
+        'Payoff Ratio/Factor': "{:.2f}",
+        'Mathematical Expectation': "{:.2f}",
+        'Max Duration': "{:.0f}",
+        'Average Duration': "{:.0f}",
+        'Min Duration': "{:.0f}",
+        # avg_stop_value / avg_stop_%
+        # Gain trades
+        'Number of Gain Trades': "{:.0f}",
+        'Win Rate %': "{:.2f}",
+        'Max Profit Value': "{:.2f}",
+        'Average Profit Value': "{:.2f}",
+        'Min Profit Value': "{:.2f}",
+        'Max Profit %': "{:.2f}",
+        'Average Profit %': "{:.2f}",
+        'Min Profit %': "{:.2f}",
+        'Max Win Trade Duration': "{:.0f}",
+        'Average Win Trade Duration': "{:.0f}",
+        'Min Win Trade Duration': "{:.0f}",
+        'Max Consecutive Wins': "{:.0f}",
+        'Average Consecutive Wins': "{:.2f}",
+        # Loss trades
+        'Number of Loss Trades': "{:.0f}",
+        'Loss Rate %': "{:.2f}",
+        'Max Loss Value': "{:.2f}",
+        'Average Loss Value': "{:.2f}",
+        'Min Loss Value': "{:.2f}",
+        'Max Loss %': "{:.2f}",
+        'Average Loss %': "{:.2f}",
+        'Min Loss %': "{:.2f}",
+        'Max Loss Trade Duration': "{:.0f}",
+        'Average Loss Trade Duration': "{:.0f}",
+        'Min Loss Trade Duration': "{:.0f}",
+        'Max Consecutive Losses': "{:.0f}",
+        'Average Consecutive Losses': "{:.2f}",
+        # Drawdown
+        'Maximum Drawdown Value': "{:.2f}",
+        'Maximum Drawdown %': "{:.2f}",
+        'Maximum Drawdown Date': "{:}",
+        'Maximum Drawdown Duration': "{:.2f}",
+        'Average Peak Drawdown %': "{:.2f}",
+        'Average Drawdown Duration': "{:.2f}",
+        'Recovery Factor': "{:.2f}"}
+    ))
+
+axis = all_stocks_setup_report.columns.to_list()
+
+c1, c2 = st.columns((2, 8))
+with c1:
+    st.write("Escolha as estatísticas que serão comparadas")
+    inp_axis_y = st.selectbox('Eixo Y', report_columns.values(), index=6)
+    axis_y = str(list(report_columns.keys())[list(report_columns.values()).index(inp_axis_y)])
+    inp_axis_x = st.selectbox('Eixo X', report_columns.values(), index=48)
+    axis_x = str(list(report_columns.keys())[list(report_columns.values()).index(inp_axis_x)])
+with c2:
+    graph_compare_report(all_stocks_setup_report, axis_x, axis_y)
