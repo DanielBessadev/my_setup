bfea99df3460ca1524b32749011bbec08108d69b
DanielBessadev <danielbessadev@gmail.com>
2023-04-25 13:10:49 -0300

    trades_in_app

--- app/pages/Ativo_e_Setup.py
+++ app/pages/Ativo_e_Setup.py
@@ -29,10 +29,10 @@ with c4:
 with c5:
     if (folder == 'wk'):
         inp_setup = st.selectbox('Setup', setups_info.keys())
-        setup = str(setups_info[inp_setup] + '_wk')
+        setup = str(setups_info[inp_setup])
     elif (folder == 'd'):
         inp_setup = st.selectbox('Setup', setups_info.keys())
-        setup = str(setups_info[inp_setup] + '_d')
+        setup = str(setups_info[inp_setup])
 with c6:
     start_capital = st.number_input(label='Capital Inicial', value=10000, step=1000, format='%d')
 with c7:
